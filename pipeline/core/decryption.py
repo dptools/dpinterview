7a7e7ff2fae163dddc670d1fce17e6251fd92885
Dheshan Mohandass <dheshan@mohandass.com>
2024-10-04 13:59:09 -0400

    chore: Remove redundant code
    Signed-off-by: Dheshan Mohandass <dheshan@mohandass.com>

--- pipeline/core/decryption.py
+++ pipeline/core/decryption.py
@@ -9,35 +9,9 @@ from pathlib import Path
 import cryptease as crypt
 
 from pipeline.helpers import utils
+from pipeline.helpers.config import get_key_from_config_file
 
 logger = logging.getLogger(__name__)
-
-
-def get_key_from_config_file(config_file: Path) -> str:
-    """
-    Retrieves the decryption key from the specified config file.
-
-    Args:
-        config_file (Path): The path to the config file.
-
-    Returns:
-        str: The decryption key.
-    """
-    # Get decryption key from config file
-    params = utils.config(config_file, section="decryption")
-    key_file = params["key_file"]
-    logger.debug(f"Using decryption key from: {key_file}")
-
-    # Check if key_file exists
-    if not Path(key_file).exists():
-        logger.error(f"Error: key_file '{key_file}' does not exist.")
-        sys.exit(1)
-
-    # Get key from key_file
-    with open(key_file, "r", encoding="utf-8") as f:
-        key = f.read().strip()
-
-    return key
 
 
 def decrypt_file(
@@ -55,7 +29,7 @@ def decrypt_file(
         None
     """
     # Decrypt file
-    key = get_key_from_config_file(config_file=config_file)
+    key = get_key_from_config_file(config_file=config_file, section="decryption")
 
     with open(file_to_decrypt, "rb") as f:
         key = crypt.key_from_file(f, key)
