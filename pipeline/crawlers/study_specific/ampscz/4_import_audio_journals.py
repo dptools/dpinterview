826fbfc557ad326f8045c9669fb6ae73befe816b
Dheshan Mohandass <dmohandass@mgh.harvard.edu>
2025-06-30 14:03:33 +0000

    feat: Add timezone handling and JSON timestamp extraction for audio journals

--- pipeline/crawlers/study_specific/ampscz/4_import_audio_journals.py
+++ pipeline/crawlers/study_specific/ampscz/4_import_audio_journals.py
@@ -21,20 +21,23 @@ try:
 except ValueError:
     pass
 
-import logging
 import argparse
-from typing import List, Tuple
-from datetime import datetime
+import json
+import logging
 import multiprocessing
+import re
+from datetime import datetime
+from typing import Dict, List, Tuple, Optional
 
+import pytz
 from rich.logging import RichHandler
 from rich.progress import Progress
 
 from pipeline import core, orchestrator
-from pipeline.helpers import cli, utils, db, dpdash
-from pipeline.models.subjects import Subject
-from pipeline.models.files import File
+from pipeline.helpers import cli, db, dpdash, utils
 from pipeline.models.audio_journals import AudioJournal
+from pipeline.models.files import File
+from pipeline.models.subjects import Subject
 
 MODULE_NAME = "import_audio_journals"
 INSTANCE_NAME = MODULE_NAME
@@ -49,6 +52,71 @@ logargs = {
 logging.basicConfig(**logargs)
 
 console = utils.get_console()
+
+
+study_timezones: Dict[str, str] = {
+    "PronetBI": "America/New_York",
+    "PronetCA": "Canada/Mountain",
+    "PronetCM": "Europe/London",
+    "PronetGA": "America/New_York",
+    "PronetHA": "America/New_York",
+    "PronetIR": "America/Los_Angeles",
+    "PronetKC": "Europe/London",
+    "PronetLA": "America/Los_Angeles",
+    "PronetMA": "Europe/Madrid",
+    "PronetMT": "Canada/Eastern",
+    "PronetMU": "Europe/Berlin",
+    "PronetNC": "America/New_York",
+    "PronetNL": "America/New_York",
+    "PronetNN": "America/Chicago",
+    "PronetOR": "America/Los_Angeles",
+    "PronetPA": "America/New_York",
+    "PronetPI": "America/New_York",
+    "PronetPV": "Europe/Rome",
+    "PronetSD": "America/Los_Angeles",
+    "PronetSF": "America/Los_Angeles",
+    "PronetSH": "Asia/Shanghai",
+    "PronetSI": "America/New_York",
+    "PronetSL": "Asia/Seoul",
+    "PronetTE": "America/New_York",
+    "PronetUR": "America/New_York",
+    "PronetWU": "US/Central",
+    "PronetYA": "America/New_York",
+}
+
+
+def get_journal_timestamp_from_mindlamp_json(
+    audio_journal: Path,
+    json_file_path: Path,
+    timezone: str
+) -> Optional[datetime]:
+    """
+    Get the timestamp from the Mindlamp JSON file.
+
+    Args:
+        audio_journal (Path): The path to the audio journal file.
+        json_file_path (Path): The path to the JSON file.
+        timezone (str): The timezone of the study.
+
+    Returns:
+        Optional[datetime]: The timestamp of the audio journal.
+    """
+    audio_journal_timestamp = None
+    with open(json_file_path, "r", encoding="utf-8") as json_file:
+        json_data = json.load(json_file)
+
+        for event in json_data:
+            if "static_data" in event and "url" in event["static_data"]:
+                url: str = event["static_data"]["url"]
+                if url.lower() in audio_journal.name.lower():
+                    unix_timestamp = event["timestamp"]  # 1739593798406
+                    audio_journal_timestamp = datetime.fromtimestamp(
+                        unix_timestamp / 1000,
+                        tz=pytz.timezone(timezone),
+                    )
+                    break
+
+    return audio_journal_timestamp
 
 
 def fetch_journals(
@@ -71,21 +139,33 @@ def fetch_journals(
     audio_journal_root_path = study_path / "raw" / subject_id / "phone"
 
     if not audio_journal_root_path.exists():
-        logger.warning(f"No audio journal path for {subject_id} at {audio_journal_root_path}")
+        logger.debug(
+            f"No audio journal path for {subject_id} at {audio_journal_root_path}"
+        )
         return []
 
     audio_journal_path = list(audio_journal_root_path.glob("*_sound_*.mp3"))
     audio_journals: List[AudioJournal] = []
 
+    study_timezone = study_timezones.get(study_id, None)
+    if study_timezone is None:
+        logger.error(f"No timezone found for {study_id}")
+        sys.exit(1)
+
     subject_consent_date = Subject.get_consent_date(
         study_id=study_id, subject_id=subject_id, config_file=config_file
     )
+    subject_consent_date = pytz.timezone(study_timezone).localize(
+        subject_consent_date  # type: ignore
+    )  # convert to timezone aware datetime
     if subject_consent_date is None:
         logger.error(f"No consent date found for {subject_id} - skipping...")
         return []
 
     if len(audio_journal_path) == 0:
-        logger.warning(f"No audio journal found for {subject_id} at {audio_journal_root_path}")
+        logger.debug(
+            f"No audio journal found for {subject_id} at {audio_journal_root_path}"
+        )
         return []
     else:
         logger.info(f"Found {len(audio_journal_path)} audio journals for {subject_id}")
@@ -95,14 +175,39 @@ def fetch_journals(
             audio_journal.name
         )  # U3121181823_PronetYA_activity_2025_01_07_sound_0.mp3
 
+        # get relevant JSON file replacing *sound_*.mp3 *.json
+        # U3121181823_PronetYA_activity_2025_01_07.json
+
+        json_file_name = re.sub(r"_sound_\d+\.mp3$", ".json", audio_journal.name)
+        json_file_path = audio_journal.parent / json_file_name
+        if not json_file_path.exists():
+            logger.error(
+                f"No JSON file found for {audio_journal} at {json_file_path}. Skipping..."
+            )
+            continue
+
+        audio_journal_timestamp = get_journal_timestamp_from_mindlamp_json(
+            audio_journal=audio_journal,
+            json_file_path=json_file_path,
+            timezone=study_timezone,
+        )
+
         audio_journal_parts = audio_journal_basename.split("_")
-        journal_date: datetime = datetime.strptime(
-            f"{audio_journal_parts[3]}-{audio_journal_parts[4]}-{audio_journal_parts[5]}",
-            "%Y-%m-%d",
-        )
-        journal_day = dpdash.get_days_between_dates(
-            consent_date=subject_consent_date, event_date=journal_date
-        )
+        if audio_journal_timestamp is None:
+            audio_journal_timestamp = datetime.strptime(
+                f"{audio_journal_parts[3]}-{audio_journal_parts[4]}-{audio_journal_parts[5]}",
+                "%Y-%m-%d",
+            )
+            audio_journal_timestamp = pytz.timezone(study_timezone).localize(
+                audio_journal_timestamp
+            )
+            logger.warning(
+                f"No timestamp found for {audio_journal} at "
+                f"{json_file_path}. Using date from filename: {audio_journal_timestamp}"
+            )
+
+        journal_day = (audio_journal_timestamp - subject_consent_date).days + 1
+
         journal_session = audio_journal_parts[-1].split(".")[0]
         journal_session = int(journal_session) + 1
 
@@ -111,13 +216,13 @@ def fetch_journals(
             subject=subject_id,
             data_type="audioJournal",
             consent_date=subject_consent_date,
-            event_date=journal_date,
+            time_range=f"day{journal_day:04d}",
         )
-        audio_journal_name = f"{audio_journal_name}-session{journal_session:04d}"
+        # audio_journal_name = f"{audio_journal_name}-session{journal_session:04d}"
         audio_journal = AudioJournal(
             aj_path=audio_journal,
             aj_name=audio_journal_name,
-            aj_date=journal_date,
+            aj_datetime=audio_journal_timestamp,
             aj_day=journal_day,
             aj_session=journal_session,
             subject_id=subject_id,
@@ -125,7 +230,33 @@ def fetch_journals(
         )
         audio_journals.append(audio_journal)
 
-    return audio_journals
+    audio_journals.sort(key=lambda journal: journal.aj_datetime)
+    unique_days = sorted(set(journal.aj_day for journal in audio_journals))
+
+    # Mark session numbers in ascending order, restarting from 1 for each day
+    # Fizes session numbers after timezone adjustment
+    sorted_journals: List[AudioJournal] = []
+    for day in unique_days:
+        day_journals = [journal for journal in audio_journals if journal.aj_day == day]
+
+        # Check if no time components, if time is 00:00:00, skip reset
+        if any(
+            journal.aj_datetime.time() == datetime.min.time()
+            for journal in day_journals
+        ):
+            logger.warning(
+                f"Skipping session number reset for {subject_id} on day {day} - No time stamp"
+            )
+            continue
+
+        session_number = 1
+        for journal in day_journals:
+            journal.aj_session = session_number
+            session_number += 1
+            journal.aj_name = f"{journal.aj_name}-session{journal.aj_session:04d}"
+            sorted_journals.append(journal)
+
+    return sorted_journals
 
 
 def hash_file_worker(params: Tuple[AudioJournal, Path]) -> File:
@@ -224,7 +355,12 @@ def import_journals(config_file: Path, study_id: str, progress: Progress) -> Non
     )
 
     # Execute the queries
-    db.execute_queries(queries=sql_queries, config_file=config_file, silent=True)
+    db.execute_queries(
+        queries=sql_queries,
+        config_file=config_file,
+        silent=True,
+        on_failure=lambda: (logger.error("Error executing queries")),
+    )
 
 
 if __name__ == "__main__":
