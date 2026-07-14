9551397dad0ff6ef7f6f4179f91f7a5fe8b6c7c5
rtmcard <rtmcardle@emory.edu>
2026-07-14 12:36:20 -0400

    Config for data dictionary

--- pipeline/crawlers/study_specific/ampscz/6_import_data_dictionary.py
+++ pipeline/crawlers/study_specific/ampscz/6_import_data_dictionary.py
@@ -19,13 +19,14 @@ try:
 except ValueError:
     pass
 
+import argparse
 import logging
 import re
 
 import pandas as pd
 from rich.logging import RichHandler
 
-from pipeline.helpers import db, utils
+from pipeline.helpers import cli, db, utils
 
 MODULE_NAME = "crawlers.import_data_dictionary"
 
@@ -59,9 +60,27 @@ def remove_html_tags(input_string: str) -> str:
 
 
 if __name__ == "__main__":
-    console.rule(f"[bold red]{MODULE_NAME}")
+    parser = argparse.ArgumentParser(
+        prog=MODULE_NAME, description="Import the data dictionary into the database."
+    )
+    parser.add_argument(
+        "-c", "--config", type=str, help="Path to the config file.", required=False
+    )
 
-    config_file = utils.get_config_file_path()
+    args = parser.parse_args()
+
+    if args.config:
+        config_file = Path(args.config).resolve()
+        if not config_file.exists():
+            console.log(f"[red]Error: Config file '{config_file}' does not exist.")
+            sys.exit(1)
+    else:
+        if cli.confirm_action("Using default config file."):
+            config_file = utils.get_config_file_path()
+        else:
+            sys.exit(1)
+
+    console.rule(f"[bold red]{MODULE_NAME}")
     console.print(f"Using config file: {config_file}")
 
     utils.configure_logging(
